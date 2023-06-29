3d26e219746123c1838539fac941aabaea11bd83
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-29 20:08:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/apojkgj09/much50")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/WE773533700/ssssssssssseeo")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
